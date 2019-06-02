4f2c6f3d0ac42822fc5d6f2a5d2e74ba4430e5df
wangding <408542507@qq.com>
2019-06-02 14:32:33 +0800

    add gitbook with gtalk plugin

---
+++ gitbook/gitbook/gitbook-plugin-gtalk/gtalk.js
@@ -0,0 +1,30 @@
+module.exports = function(book, page) {
+  var cfg = book.config.get('pluginsConfig')['gtalk'];
+  cfg.id = page.path.slice(0, page.path.length-2) + 'html';
+
+  var DOM = '\n\n'
+    + '<div id="gitalk-container"></div>'
+    + '<script src="https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js"></script>'
+    + '<script>'
+      + 'window.onload = function() {'
+        + 'gitbook.events.on("page.change", function() {'
+          + 'var gitalk;'
+          + 'if(typeof gitalk === "undefined") {'
+            + 'gitalk = new Gitalk(' + JSON.stringify(cfg) + ');'
+          + '}'
+          + 'gitalk.render("gitalk-container");'
+        + '});'
+      + '};'
+      + 'var gitalk;'
+      + 'if(typeof gitalk === "undefined") {'
+        + 'gitalk = new Gitalk(' + JSON.stringify(cfg) + ');'
+      + '}'
+      + 'gitalk.render("gitalk-container");'
+    + '</script>';
+
+  page.content += DOM;
+
+  console.log(page.content);
+
+  return page;
+};
